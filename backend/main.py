0002cd1d92c55b2c11817a4d13eb3108a6e6d92c
edenv44 <101055313+edenv44@users.noreply.github.com>
2025-11-12 00:03:16 +0200

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from fastapi import FastAPI, HTTPException, Depends, Body
 import uvicorn
@@ -538,6 +539,9 @@ def remove_user_from_main_task(main_task_id: int, user_id: int):
 
 
 
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
